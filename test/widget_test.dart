e9abb26021d727e8ff6690e5a5b7a1beef0b1485
sadeqmoradzade-dotcom <sadeqmoradzade@gmail.com>
2026-09-18 23:57:31 +0330

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vakil/main.dart';
+
+void main() {
+  testWidgets(
+    'Vakil app starts correctly',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(const VakilApp());
+
+      expect(find.text('وکیل'), findsOneWidget);
+      expect(
+        find.text('عبارت موردنظر را وارد کنید'),
+        findsOneWidget,
+      );
+    },
+  );
+}
